f78beff45ea5932e367c88dee8adeec9db9abca6
Enkhsaikhan <Ssundeebb@gmail.com>
2022-06-24 19:09:59 +0800

    BlackJack2 done for command line only next gonna try to do oop shit

--- python files/cards.py
+++ python files/cards.py
@@ -1,23 +1,25 @@
 import random
 
+
 class Cards(object):
     def __init__(self, suit, val) -> None:
         self.suit = suit
         self.val = val
-    
+
     def show(self):
         print(f"{self.val} of {self.suit}")
+
 
 class Deck(object):
     def __init__(self) -> None:
         self.card = []
         self.build()
-    
+
     def build(self):
         for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
             for v in range(1, 14):
                 self.card.append(Cards(s, v))
-    
+
     def show(self):
         for c in self.card:
             c.show()
@@ -26,9 +28,10 @@ class Deck(object):
         for i in range(len(self.card) - 1, 0, -1):
             r = random.randint(0, i)
             self.card[i], self.card[r] = self.card[r], self.card[i]
-    
+
     def drawCard(self):
         return self.card.pop()
+
 
 class Player(object):
     def __init__(self, name):
@@ -42,9 +45,10 @@ class Player(object):
     def showHand(self):
         for card in self.hand:
             card.show()
-    
+
     def discard(self):
         return self.hand.pop()
+
 
 deck = Deck()
 deck.shuffle()
